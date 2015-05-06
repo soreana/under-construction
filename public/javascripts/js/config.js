b15798981eaaf806b2696291e5564be45aeaf1bf
Sina Kashipazha <sina@sinsing.org>
2015-05-06 09:50:04 -0400

    add reactive ribon

--- public/javascripts/js/config.js
+++ public/javascripts/js/config.js
@@ -1,5 +1,5 @@
 // Insert countdown's end date in 'YYYY/mm/dd' (Year/Month/Day) format.
-end_date = '2017/06/25';
+end_date = '2015/05/21';
 
 // Insert countdown's end time in 'HH:ii:ss' (Hour:Minute:Second) format.
-end_time = '18:35:00';
+end_time = '14:00:00';
